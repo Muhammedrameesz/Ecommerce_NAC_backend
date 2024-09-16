8a63edacb00a17d7638c39c0918c0321fd95dfb7
Muhammedrameesz <rameezta555@gmail.com>
2024-09-16 12:24:49 +0530

    final commit

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = new express();
-const morgan = require('morgan');
-require('dotenv').config()
-const PORT = process.env.PORT || 5200
-const cors = require('cors')
-const DBconnection = require('./DB/DBconnect')
-const router = require('./Routes')
+const morgan = require("morgan");
+require("dotenv").config();
+const PORT = process.env.PORT || 5200;
+const cors = require("cors");
+const DBconnection = require("./DB/DBconnect");
+const router = require("./Routes");
 
-
+const allowedOrigins = [
+  "https://ecommerce-nac-frontend.vercel.app",
+  "https://ecommerce-nac-frontend-git-main-muhammedrameeszs-projects.vercel.app",
+  "https://ecommerce-nac-frontend-mi3e7ybt7-muhammedrameeszs-projects.vercel.app",
+];
 
 DBconnection();
 
-app.use(cors(
-    {
-        origin: '*',
-
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-))
+}));
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use('/', router)
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", router);
 
-
-app.all('*', (req, res) => {
-    res.status(404).send('404 not found')
-})
+app.all("*", (req, res) => {
+  res.status(404).send("404 not found");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
