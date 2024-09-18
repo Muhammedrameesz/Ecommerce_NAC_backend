f0ca41ecf8b21f25e80365907b65ea71a2a09bc3
Muhammedrameesz <rameezta555@gmail.com>
2024-09-18 17:08:06 +0530

    final commit

--- controllers/instructerController.js
+++ controllers/instructerController.js
@@ -5,20 +5,17 @@ const instructer =async (req,res)=>{
         try {
             const {email,password}=await req.body
             if(email==='admin@gmail.com' && password==='password'){
-                
                 const saltRound = 10
                 const hashPassword =await bcrypt.hash(password, saltRound)
-                console.log("password:",hashPassword);
-
                 const tokens = generateToken(email)
-                console.log('token:',tokens);
                 return res.status(200).send({status:true,messege:'success',tokens:tokens})
             }else{
-                return res.status(200).send({status:false,messege:'invalid email or password'})
+                return res.status(400).send({status:false,messege:'invalid email or password'})
             }
             
         } catch (error) {
             console.log(error);
+            res.status(500).json({message:'internal server error'})
         }
 }
 
